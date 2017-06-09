761d2806d6bbd897ffa8663e9036790c7e3647fb
it43g2015 <it43g2015@WS3.itc03.edu>
2017-06-09 13:14:00 +0200

    IT 43/2015

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,7 +1,7 @@
 
 // Before you commit and push write your student ID and finish time here.
-// Finish time:
-// Student ID:
+// Finish time: 13:00
+// Student ID: IT 43/2015
 
 public class PlanetExplorer {
 	private int x,y;
